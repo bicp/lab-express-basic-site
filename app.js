38f62899a81b9447b8c4211601798f91a6fe3eb5
Bianca Pimenta <bi.cp1802@gmail.com>
2022-01-18 00:25:13 +0100

    lab done

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "views/home.html");
+  res.sendFile(__dirname + "/views/home.html");
 });
 app.get("/about", (req, res) => {
-  res.sendFile(__dirname + "views/about.html");
+  res.sendFile(__dirname + "/views/about.html");
 });
 app.get("/works", (req, res) => {
-  res.sendFile(__dirname + "views/works.html");
+  res.sendFile(__dirname + "/views/work.html");
 });
+
+app.use(express.static("public"));
 
 app.listen(5001, () => {
   console.log("Server is running");
